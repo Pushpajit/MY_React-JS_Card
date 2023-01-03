0897274ff59b837b16c2a134a7c4db1030abc981
Pushpajit Biswas <pushpajitnexus007@gmail.com>
2023-01-03 18:57:55 +0530

    Inital commit for mycard

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,12 +6,14 @@ import linkedln from '../images/linkedin.png'
 
 function Footer() {
   return (
-    <footer>
-        <a href="#"><img src={meta} alt="meta" /></a>
-        <a href="https://github.com/Pushpajit?tab=repositories"><img src={github} alt="github" /></a>
-        <a href="https://www.instagram.com/pushpajitbiswas/"><img src={insta} alt="insta" /></a>
-        <a href="https://www.linkedin.com/in/pushpajit-biswas-6928b715b/"><img src={linkedln} alt="linkedln" /></a>
-    </footer>
+    <div className="footer-div">
+      <footer>
+          <a href="#"><img src={meta} alt="meta" /></a>
+          <a href="https://github.com/Pushpajit?tab=repositories"><img src={github} alt="github" /></a>
+          <a href="https://www.instagram.com/pushpajitbiswas/"><img src={insta} alt="insta" /></a>
+          <a href="https://www.linkedin.com/in/pushpajit-biswas-6928b715b/"><img src={linkedln} alt="linkedln" /></a>
+      </footer>
+    </div>
   )
 }
 
